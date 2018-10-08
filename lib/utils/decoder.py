e341d167c5279e146c58a419dc07741ea1dc76e2
cclauss <cclauss@bluewin.ch>
2018-10-08 17:33:32 +0200

    Define unicode in Python 3
    __unicode()__ was removed in Python 3 because all __str__ are Unicode.

--- lib/utils/decoder.py
+++ lib/utils/decoder.py
@@ -5,9 +5,16 @@
 # @descr : Web Application Audit Framework
 # @author: Momo Outaadi
 
+try:
+	unicode        # Python 2
+except NameError:
+	unicode = str  # Python 2
+
+
 def to_str(obj):
 	if not isinstance(obj,str):
 		return str(obj)
+
 
 def to_unicode(obj):
 	if not isinstance(obj,unicode):
